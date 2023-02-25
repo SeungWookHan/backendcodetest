eb6d2eb8c7fb2804ba75acf0bafdec2b7f7938f3
SeungWookHan <hswook12@me.com>
2023-02-25 16:21:13 +0900

    Add service logic onto controller

--- src/api/routes/bus.ts
+++ src/api/routes/bus.ts
@@ -1,4 +1,7 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+import { Container } from "typedi";
+import { Logger } from "winston";
+import BusService from "services/bus";
 
 /**
  * @route /buses
@@ -7,6 +10,23 @@ const route: Router = Router();
 
 route.get("/test", async (req: Request, res: Response) => {
   return res.status(200).send("This is test response");
+});
+
+route.get("/buses", async (req: Request, res: Response, next: NextFunction) => {
+  const logger: Logger = Container.get("logger");
+  logger.debug("Calling /buses endpoint to get busInfo");
+  try {
+    const busService = Container.get(BusService);
+    const routes = await busService.getRandomRoutes();
+    const busStops = await busService.getBusStops(
+      routes.map((r) => r.busRouteId)
+    );
+    const busArrivals = await busService.getFastestBusArrivals(busStops);
+    return res.status(200).json(busArrivals);
+  } catch (e) {
+    logger.error("🔥 Fail to get busInfo: %o", e);
+    return next(e);
+  }
 });
 
 export default route;
