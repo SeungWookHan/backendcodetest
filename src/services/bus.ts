645d26822055e3cb23f47a173c4c93b8afccb784
SeungWookHan <hswook12@me.com>
2023-02-25 16:32:44 +0900

    Add try catch for exeption check

--- src/services/bus.ts
+++ src/services/bus.ts
@@ -25,67 +25,85 @@ export default class BusService {
   constructor(@Inject("logger") private logger) {}
 
   public async getRandomRoutes(): Promise<IBusRoute[]> {
-    const url = `${BUS_API_URL}/busRouteInfo/getBusRouteList?ServiceKey=${config.accessKey}&strSrch=`;
-    const response = await axios.get(url);
-    const busRoutes = response.data.msgBody.busRouteList;
+    this.logger.silly("Get busRoutes");
+    try {
+      const url = `${BUS_API_URL}/busRouteInfo/getBusRouteList?ServiceKey=${config.accessKey}&strSrch=`;
+      const response = await axios.get(url);
+      const busRoutes = response.data.msgBody.busRouteList;
 
-    const selectedRoutes: IBusRoute[] = [];
-    while (selectedRoutes.length < 3) {
-      const randomIndex = Math.floor(Math.random() * busRoutes.length);
-      const selectedRoute = busRoutes[randomIndex];
-      if (!selectedRoutes.includes(selectedRoute)) {
-        selectedRoutes.push(selectedRoute);
+      const selectedRoutes: IBusRoute[] = [];
+      while (selectedRoutes.length < 3) {
+        const randomIndex = Math.floor(Math.random() * busRoutes.length);
+        const selectedRoute = busRoutes[randomIndex];
+        if (!selectedRoutes.includes(selectedRoute)) {
+          selectedRoutes.push(selectedRoute);
+        }
       }
+      return selectedRoutes;
+    } catch (e) {
+      this.logger.error("🔥 Fail to get busRoutes: %o", e);
+      throw e;
     }
-    return selectedRoutes;
   }
 
   public async getBusStops(busRoutes: string[]): Promise<IBusStop[]> {
-    const busStops: IBusStop[] = [];
+    this.logger.silly("Get busStops");
+    try {
+      const busStops: IBusStop[] = [];
 
-    for (const routeId of busRoutes) {
-      const url = `${BUS_API_URL}/arrive/getArrInfoByRouteAll?ServiceKey=${config.accessKey}&busRouteId=${routeId}`;
-      const response = await axios.get(url);
-      const stops = response.data.msgBody.stations;
+      for (const routeId of busRoutes) {
+        const url = `${BUS_API_URL}/arrive/getArrInfoByRouteAll?ServiceKey=${config.accessKey}&busRouteId=${routeId}`;
+        const response = await axios.get(url);
+        const stops = response.data.msgBody.stations;
 
-      // Get bus stop with most scheduled buses to arrive in 5 minutes
-      const selectedStop = stops.reduce(
-        (prev, curr) => {
-          const currBuses =
-            Number(curr.traTime1) + Number(curr.traTime2) <= 5
-              ? Number(curr.arrmsg1) + Number(curr.arrmsg2)
-              : 0;
-          return currBuses > prev.buses
-            ? { stopId: curr.station, buses: currBuses }
-            : prev;
-        },
-        { stopId: "", buses: 0 }
-      );
+        // Get bus stop with most scheduled buses to arrive in 5 minutes
+        const selectedStop = stops.reduce(
+          (prev, curr) => {
+            const currBuses =
+              Number(curr.traTime1) + Number(curr.traTime2) <= 5
+                ? Number(curr.arrmsg1) + Number(curr.arrmsg2)
+                : 0;
+            return currBuses > prev.buses
+              ? { stopId: curr.station, buses: currBuses }
+              : prev;
+          },
+          { stopId: "", buses: 0 }
+        );
 
-      busStops.push({ routeId, stopId: selectedStop.stopId });
+        busStops.push({ routeId, stopId: selectedStop.stopId });
+      }
+
+      return busStops;
+    } catch (e) {
+      this.logger.error("🔥 Fail to get busStops: %o", e);
+      throw e;
     }
-
-    return busStops;
   }
 
   public async getFastestBusArrivals(busStops: IBusStop[]): Promise<IBusETA[]> {
-    const busArrivals: IBusETA[] = [];
+    this.logger.silly("Get busArrivals");
+    try {
+      const busArrivals: IBusETA[] = [];
 
-    for (const stop of busStops) {
-      const url = `${BUS_API_URL}/arrive/getArrInfoByRouteList?ServiceKey=${config.accessKey}&stId=${stop.stopId}&busRouteId=${stop.routeId}&ord=1`;
-      const response = await axios.get(url);
-      const arrivals = response.data.msgBody.itemList;
+      for (const stop of busStops) {
+        const url = `${BUS_API_URL}/arrive/getArrInfoByRouteList?ServiceKey=${config.accessKey}&stId=${stop.stopId}&busRouteId=${stop.routeId}&ord=1`;
+        const response = await axios.get(url);
+        const arrivals = response.data.msgBody.itemList;
 
-      arrivals.forEach((arrival) => {
-        busArrivals.push({
-          routeId: stop.routeId,
-          busNumber: arrival.rtNm,
-          plateNumber: arrival.plainNo1,
-          eta: arrival.traTime1,
+        arrivals.forEach((arrival) => {
+          busArrivals.push({
+            routeId: stop.routeId,
+            busNumber: arrival.rtNm,
+            plateNumber: arrival.plainNo1,
+            eta: arrival.traTime1,
+          });
         });
-      });
-    }
+      }
 
-    return busArrivals.sort((a, b) => a.eta - b.eta);
+      return busArrivals.sort((a, b) => a.eta - b.eta);
+    } catch (e) {
+      this.logger.error("🔥 Fail to get busArrivals: %o", e);
+      throw e;
+    }
   }
 }
